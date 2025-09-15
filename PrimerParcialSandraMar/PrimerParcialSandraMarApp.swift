36428d29c0edaa4e24525c3c4076519003c0a7a2
Sandra <sandra.mar05@iest.edu.mx>
2025-09-15 17:36:50 -0600

    Initial Commit

---
+++ PrimerParcialSandraMar/PrimerParcialSandraMarApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimerParcialSandraMarApp.swift
+//  PrimerParcialSandraMar
+//
+//  Created by WIN603 on 15/09/25.
+//
+
+import SwiftUI
+
+@main
+struct PrimerParcialSandraMarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
